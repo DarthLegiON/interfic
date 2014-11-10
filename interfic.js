155504f1c118b2766c9d124da6d33a8cd025079e
Anthony Legos <legion.dart@gmail.com>
2014-11-10 19:04:00 +0300

    инициализация игры

--- interfic.js
+++ interfic.js
@@ -1,12 +1,22 @@
-var Game = Object();
-
-function Game(quest)
+function Game(questCode)
 {
-    this.name = quest.name;
-
+    this._reQuest(questCode);
 }
 
-$(document).ready(function ()
+Game.prototype.init = function ()
 {
-    
-});
+    alert(Quest.name);
+}
+
+Game.prototype._reQuest = function (questCode)
+{
+    var script = document.createElement('script');
+    script.src = 'quests/' + questCode + '/quest.js';
+    script.charset = 'UTF-8';
+    var _this = this;
+    script.onload = function ()
+    {
+        _this.init(Quest);
+    };
+    document.head.appendChild(script);
+};
