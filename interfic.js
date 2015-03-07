b5b26f4fbca7968432f502c0abcce8ea34ee0eef
Anthony Legos <legion.dart@gmail.com>
2015-03-08 01:51:43 +0300

    комментарии, чистка кода

--- interfic.js
+++ interfic.js
@@ -1,24 +1,44 @@
+/* global Quest */
+
 var game;
 
+/**
+ * Наследует один класс от другого
+ * @param {Object} Child Класс-наследник
+ * @param {Object} Parent Класс-родитель
+ * @returns {undefined}
+ */
 function extend(Child, Parent)
 {
-    var F = function () { };
+    var F = function () {
+    };
     F.prototype = Parent.prototype;
     Child.prototype = new F();
     Child.prototype.constructor = Child;
     Child.superclass = Parent.prototype;
 }
 
-
+/**
+ * Заменяет значение параметра на умолчание, если оно undefined
+ * @param {type} param параметр
+ * @param {type} def значение по умолчанию
+ * @returns {unresolved}
+ */
 function getFuncParam(param, def)
 {
     return (param === undefined) ? def : param;
 }
 
 //-----------------------------------------------------------------------------------
+/**
+ * Класс "Игра", основной класс.
+ * Конструктор начинает игру, основываясь на коде квеста
+ * @param {type} questCode код квеста
+ * @returns {Game}
+ */
 function Game(questCode)
 {
-    
+
     this._text = [];
     this._answers = [new Answer()];
     this._picture = null;
@@ -27,9 +47,13 @@ function Game(questCode)
     this.init = document.createEvent('HTMLEvents');
     this.init.initEvent('init');
     this._reQuest(questCode);
-    
+
 }
 
+/**
+ * Запускает квест
+ * @returns {undefined}
+ */
 Game.prototype.initialize = function ()
 {
     //alert(Quest.name);
@@ -38,6 +62,11 @@ Game.prototype.initialize = function ()
     this.showQuestInfo();
 };
 
+/**
+ * Загружает JS-код квеста
+ * @param {type} questCode
+ * @returns {undefined}
+ */
 Game.prototype._reQuest = function (questCode)
 {
     var script = document.createElement('script');
@@ -51,6 +80,10 @@ Game.prototype._reQuest = function (questCode)
     document.head.appendChild(script);
 };
 
+/**
+ * Отображает информацию о квесте (название и версию) в специальном блоке
+ * @returns {undefined}
+ */
 Game.prototype.showQuestInfo = function ()
 {
     var questInfo = document.getElementById('questinfo');
@@ -67,34 +100,51 @@ Game.prototype.showQuestInfo = function ()
     }
 };
 
+/**
+ * Устанавливает текст в блок для текста
+ * @param {type} text
+ * @returns {undefined}
+ */
 Game.prototype.setText = function (text)
 {
     this._text = text;
     var textElement = document.getElementById('text');
-    var html = ''
+    var html = '';
     for (var i in text) {
         html += text[i].getBlock();
     }
     textElement.innerHTML = html;
 };
 
+/**
+ * Изменяет картинку в блоке для картинки
+ * @param {type} picture
+ * @returns {undefined}
+ */
 Game.prototype.setPicture = function (picture)
 {
     this._picture = picture;
     document.getElementById('picture').innerHTML = '<img src="quests/' + this._questCode + '/images/' + picture.name + '">';
 };
 
-
-
+/**
+ * Меняет состояние квеста
+ * @param {type} stageId ID состояния
+ * @returns {undefined}
+ */
 Game.prototype.setStage = function (stageId)
 {
     this._currentStage = stageId;
     this.setText(Quest.stages[stageId].getTexts());
-	this.setPicture(Quest.pictures[Quest.stages[stageId].pictureid]);
-	this.setAnswers();
-	this.setParams();
+    this.setPicture(Quest.pictures[Quest.stages[stageId].pictureid]);
+    this.setAnswers();
+    this.setParams();
 };
 
+/**
+ * Завершает квест
+ * @returns {undefined}
+ */
 Game.prototype.finish = function ()
 {
     if (confirm('Игра закончена! Попробовать еще раз?')) {
@@ -102,29 +152,37 @@ Game.prototype.finish = function ()
     } else {
         // do smth
     }
-}
-
-Game.prototype.setAnswers = function ()
-{
-	this._answers = Quest.stages[this._currentStage].answers;
-	var answersElement = document.getElementById('answers');
-	answersElement.innerHTML = '';
-	for (var i in this._answers) {
-		var answer = Quest.answers[this._answers[i]];
-		var answerElement = document.createElement('a');
-		answerElement.className = 'answer';
-		if (answer.active) {
-			answerElement.classList.add('active');
-			answerElement.href = 'javascript://';
-			answerElement.onclick = Quest.actions[answer.actionid];
-		}
-		answerElement.innerHTML = answer.text;
-		answersElement.appendChild(answerElement);
-		//html += answerElement.outerHTML;
-	}
-	//answersElement.innerHTML = html;
 };
 
+/**
+ * Отображает варианты действий
+ * @returns {undefined}
+ */
+Game.prototype.setAnswers = function ()
+{
+    this._answers = Quest.stages[this._currentStage].answers;
+    var answersElement = document.getElementById('answers');
+    answersElement.innerHTML = '';
+    for (var i in this._answers) {
+        var answer = Quest.answers[this._answers[i]];
+        var answerElement = document.createElement('a');
+        answerElement.className = 'answer';
+        if (answer.active) {
+            answerElement.classList.add('active');
+            answerElement.href = 'javascript://';
+            answerElement.onclick = Quest.actions[answer.actionid];
+        }
+        answerElement.innerHTML = answer.text;
+        answersElement.appendChild(answerElement);
+        //html += answerElement.outerHTML;
+    }
+    //answersElement.innerHTML = html;
+};
+
+/**
+ * Отображает параметры
+ * @returns {undefined}
+ */
 Game.prototype.setParams = function ()
 {
     this._params = Quest.stages[this._currentStage].params;
@@ -141,15 +199,28 @@ Game.prototype.setParams = function ()
 
 //---------------------------------------------------------------------------
 
+/**
+ * Состояние квеста. Отвечает за набор параметров, 
+ * @param {type} answers Массив ID ответов
+ * @param {type} params Массив ID параметров
+ * @param {type} type Тип (start, medium или finish, пока не используется)
+ * @param {type} texts Массив ID текстов
+ * @param {type} picture ID картинки
+ * @returns {Stage}
+ */
 function Stage(answers, params, type, texts, picture)
 {
     this.answers = getFuncParam(answers, [new Answer()]);
-	this.params = getFuncParam(params, [new Parameter()]);
+    this.params = getFuncParam(params, [new Parameter()]);
     this.type = getFuncParam(type, 'medium');
     this.texts = getFuncParam(texts, []);
-	this.pictureid = getFuncParam(picture, 0);
+    this.pictureid = getFuncParam(picture, 0);
 }
 
+/**
+ * Выдает список текстов для текущего состояния
+ * @returns {Array}
+ */
 Stage.prototype.getTexts = function ()
 {
     var returnResult = [];
@@ -161,21 +232,36 @@ Stage.prototype.getTexts = function ()
 
 //---------------------------------------------------------------------------
 
+/**
+ * Ответ, вариант действия.
+ * @param {type} text текст ответа
+ * @param {type} active активность
+ * @param {type} action ID действия
+ * @returns {Answer}
+ */
 function Answer(text, active, action)
 {
-	this.text = getFuncParam(text, 'Ответ');
-	this.active = getFuncParam(active, true);
-	this.actionid = getFuncParam(action, 0);
-	this.action = null;
+    this.text = getFuncParam(text, 'Ответ');
+    this.active = getFuncParam(active, true);
+    this.actionid = getFuncParam(action, 0);
+    this.action = null;
 }
 
+/**
+ * Задает действие для ответа
+ * @returns {undefined}
+ */
 Answer.prototype.setAction = function ()
 {
-	this.action = Quest.actions[this.actionid];
+    this.action = Quest.actions[this.actionid];
 };
 
 //---------------------------------------------------------------------------
-
+/**
+ * Картинка.
+ * @param {type} name имя файла картинки, автоматически находится в папке images квеста
+ * @returns {Picture}
+ */
 function Picture(name)
 {
     this.name = name;
@@ -183,7 +269,15 @@ function Picture(name)
 
 
 //---------------------------------------------------------------------------
-
+/**
+ * Абстрактный параметр
+ * @param {string} type тип
+ * @param {mixed} value значение по умолчанию
+ * @param {string} prefix Префикс (то, что выводится перед значением)
+ * @param {string} postfix постфикс (то, что выводится после значения)
+ * @param {boolean} hidden true, если параметр не надо отображать в блоке параметров
+ * @returns {Parameter}
+ */
 function Parameter(type, value, prefix, postfix, hidden)
 {
     this._type = type;
@@ -193,16 +287,29 @@ function Parameter(type, value, prefix, postfix, hidden)
     this.hidden = getFuncParam(hidden, false);
 }
 
+/**
+ * 
+ * @param {type} value
+ * @returns {undefined}
+ */
 Parameter.prototype.setValue = function (value)
 {
     this._value = value;
-}
+};
 
+/**
+ * 
+ * @returns {type}
+ */
 Parameter.prototype.getValue = function ()
 {
     return this._value;
-}
+};
 
+/**
+ * Перегружает метод toString. Выдает текстовое представление параметра (с префиксом, постфиксом и кодами)
+ * @returns {String}
+ */
 Parameter.prototype.toString = function ()
 {
     var string = '';
@@ -212,23 +319,44 @@ Parameter.prototype.toString = function ()
         string += (this._postfix !== null) ? ' ' + this._postfix : '';
     }
     return string;
-}
+};
 
+/**
+ * Выдает текстовое представление параметра. Перегружается в более конкретных типах параметров
+ * @returns {string}
+ */
 Parameter.prototype._getTextValue = function ()
 {
     return this._value;
-}
+};
 
+/**
+ * Показывает параметр
+ * @returns {undefined}
+ */
 Parameter.prototype.show = function ()
 {
     this.hidden = false;
-}
+};
 
+/**
+ * Скрывает параметр
+ * @returns {undefined}
+ */
 Parameter.prototype.hide = function ()
 {
     this.hidden = true;
-}
+};
 
+//-------------------------------------------------
+/**
+ * Параметр в виде текста. Фактически ничем не отличается от базового.
+ * @param {type} value
+ * @param {type} prefix
+ * @param {type} postfix
+ * @param {type} hidden
+ * @returns {undefined}
+ */
 function TextParameter(value, prefix, postfix, hidden)
 {
     TextParameter.superclass.constructor.call(this, 'text', value, prefix, postfix, hidden);
@@ -236,12 +364,28 @@ function TextParameter(value, prefix, postfix, hidden)
 
 extend(TextParameter, Parameter);
 
+//-------------------------------------------------
+/**
+ * Числовой параметр
+ * @param {type} value
+ * @param {type} prefix
+ * @param {type} postfix
+ * @param {type} rangeValues строки, соответствующие значениям параметра
+ * @param {type} hidden
+ * @returns {NumberParameter}
+ */
 function NumberParameter(value, prefix, postfix, rangeValues, hidden)
 {
     NumberParameter.superclass.constructor.call(this, 'number', value, prefix, postfix, hidden);
     this._rangeValues = getFuncParam(rangeValues, null);
 }
 
+extend(NumberParameter, Parameter);
+
+/**
+ * Перегруженый метод, @see Parameter
+ * @returns {string}
+ */
 NumberParameter.prototype._getTextValue = function ()
 {
     if (this._rangeValues !== null) {
@@ -253,8 +397,12 @@ NumberParameter.prototype._getTextValue = function ()
             return this._value.toFixed(2).toString();
         }
     }
-}
+};
 
+/**
+ * Если у параметра заданы текстовые аналоги значений, выдает их.
+ * @returns {string}
+ */
 NumberParameter.prototype._getRange = function ()
 {
     var result = this._rangeValues.default;
@@ -264,26 +412,57 @@ NumberParameter.prototype._getRange = function ()
         }
     }
     return result;
-}
+};
 
-extend(NumberParameter, Parameter);
 
 //---------------------------------------------------------------------------
-
+/**
+ * Блок текста. Может быть целым тегом, началом тега, серединой или концом.
+ * @param {string} text содержимое
+ * @param {boolean} open true, если начинается открывающимся тегом
+ * @param {boolean} close true, если заканчивается закрывающимся тегом
+ * @param {string} tag имя тега
+ * @param {string} style css-стиль
+ * @returns {TextBlock}
+ */
 function TextBlock(text, open, close, tag, style)
 {
-    this._text = (text === undefined) ? '' : text;
-    this._open = (open === undefined) ? false : open;
-    this._close = (close === undefined) ? true && (open !== undefined) : close;
-    this._tag = (tag === undefined) ? 'p' : tag;
-    this._style = (style === undefined) ? '' : style;
-    this._html = open && close;
+    this._text = getFuncParam(text, '');
+    this._open = getFuncParam(open, false);
+    this._close = getFuncParam(close, open !== undefined);
+    this._tag = getFuncParam(tag, 'p');
+    this._style = getFuncParam(style, '');
+    this._html = this._open && this._close;
 }
 
-TextBlock.prototype.isOpen = function () { return this._open && !this._close; }
-TextBlock.prototype.isClose = function () { return this._close && !this._open; }
-TextBlock.prototype.tag = function () { return this._tag; }
+/**
+ * Является ли блок только открывающимся
+ * @returns {Boolean}
+ */
+TextBlock.prototype.isOpen = function () {
+    return this._open && !this._close;
+};
 
+/**
+ * Является ли блок только закрывающимся
+ * @returns {Boolean}
+ */
+TextBlock.prototype.isClose = function () {
+    return this._close && !this._open;
+};
+
+/**
+ * Геттер для tag
+ * @returns {Boolean}
+ */
+TextBlock.prototype.tag = function () {
+    return this._tag;
+};
+
+/**
+ * Возвращает код блока вместе с тегом и содержимым
+ * @returns {String}
+ */
 TextBlock.prototype.getBlock = function ()
 {
     if (this._html) {
@@ -294,21 +473,24 @@ TextBlock.prototype.getBlock = function ()
     }
     else {
         if (this._open) {
-            return '<' + this._tag 
-                + ((this._style !== '') ? ' class="' + this._style + '" ' : '')
-                + '>' + this._text;
+            return '<' + this._tag
+                    + ((this._style !== '') ? ' class="' + this._style + '" ' : '')
+                    + '>' + this._text;
         } else if (this._close) {
             return this._text
-                + '</' + this._tag + '>';
+                    + '</' + this._tag + '>';
         } else {
             return this._text;
         }
-        
+
     }
-}
+};
 
 
-
+/**
+ * Запуск игры.
+ * @returns {undefined}
+ */
 window.onload = function ()
 {
     game = new Game('quest1');
--- quests/quest1/quest.js
+++ quests/quest1/quest.js
@@ -1,7 +1,9 @@
+/* global game */
+
 var Quest = {
-    name : 'Тестовый квест 1',
+    name: 'Тестовый квест 1',
     version: '0.0.1',
-    texts : {
+    texts: {
         0: new TextBlock('Вы прилетели на Цитадель. В доках было многолюдно, туда-сюда сновали челноки, причаливали и отчаливали корабли.', true, true),
         1: new TextBlock('посольство', true, true, 'span', 'yellow'),
         2: new TextBlock('Напротив вас стоит лифт. Он может отвезти вас в ', true, false),
@@ -11,56 +13,51 @@ var Quest = {
         6: new TextBlock('доки', true, true, 'span', 'yellow'),
         7: new TextBlock('Вы слишком долго тянули, и ожидавший вас контакт решил, что вы не явитесь на встречу. Миссия провалена', true, true)
     },
-
     stages: {
         0: new Stage([0], [0, 1], 'start', [0, 2, 1, 3], 1),
         1: new Stage([1, 2], [1], 'medium', [4, 2, 6, 3], 2),
         2: new Stage([3], [1], 'finish', [7], 2)
     },
-	
-	pictures: {
-	    0: new Picture('slider.jpg'),
-	    1: new Picture('docks.jpg'),
+    pictures: {
+        0: new Picture('slider.jpg'),
+        1: new Picture('docks.jpg'),
         2: new Picture('embassies.png')
-	},
-	
-	actions: {
-		0: function (data)
-		{
-		    Quest.parameters[1].setValue(Quest.parameters[1].getValue() + 3);
-		    if (Quest.parameters[1] !== 0) {
-		        Quest.parameters[1].show();
-		    }
-		    if (Quest.parameters[1].getValue() >= 60) {
-		        game.setStage(2);
-		    } else {
-		        game.setStage(1);
-		    }
-		},
-		1: function (data)
-		{
-		    Quest.parameters[1].setValue(Quest.parameters[1].getValue() + 3);
-		    if (Quest.parameters[1].getValue() >= 60) {
-		        game.setStage(2);
-		    } else {
-		        game.setStage(0);
-		    }
-		},
-		2: function (data)
-		{
-		    game.finish();
-		}
-	},
-	
-	answers: {
-		0: new Answer('В посольство', true, 0),
-		1: new Answer('В доки', true, 1),
-		2: new Answer('Разузнать', false, 1),
-		3: new Answer('Квест провален', true, 2),
-	},
-
-	parameters: {
-	    0: new TextParameter('Вы стоите у своего корабля'),
-	    1: new NumberParameter(0, 'Прошло', 'мин', null, true),
-	}
+    },
+    actions: {
+        0: function (data)
+        {
+            Quest.parameters[1].setValue(Quest.parameters[1].getValue() + 3);
+            if (Quest.parameters[1] !== 0) {
+                Quest.parameters[1].show();
+            }
+            if (Quest.parameters[1].getValue() >= 60) {
+                game.setStage(2);
+            } else {
+                game.setStage(1);
+            }
+        },
+        1: function (data)
+        {
+            Quest.parameters[1].setValue(Quest.parameters[1].getValue() + 3);
+            if (Quest.parameters[1].getValue() >= 60) {
+                game.setStage(2);
+            } else {
+                game.setStage(0);
+            }
+        },
+        2: function (data)
+        {
+            game.finish();
+        }
+    },
+    answers: {
+        0: new Answer('В посольство', true, 0),
+        1: new Answer('В доки', true, 1),
+        2: new Answer('Разузнать', false, 1),
+        3: new Answer('Квест провален', true, 2),
+    },
+    parameters: {
+        0: new TextParameter('Вы стоите у своего корабля'),
+        1: new NumberParameter(0, 'Прошло', 'мин', null, true),
+    },
 };
