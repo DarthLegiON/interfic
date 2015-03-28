dc3e4ac838cdce9ac839234e8862c26cd126d299
Anthony Legos <legion.dart@gmail.com>
2015-03-29 01:34:35 +0300

    objects in motion

--- editor/js/editor.js
+++ editor/js/editor.js
@@ -1,10 +1,14 @@
-/* global Menu */
+/* global Menu, Editor */
 
 function onDocumentReady()
 {
     $('input[type="checkbox"]').after('<i class="fa fa-lg"></i>');
     Menu.render();
-    Menu.jump('start');
+    if (Editor.questLoaded()) {
+        Menu.jump('quest/info', {quest: localStorage.quest});
+    } else {
+        Menu.jump('start');
+    }
 }
 
 $(document).on('ready',function () {
@@ -15,3 +19,25 @@ $(document).on('click', '.nav a:not([data-toggle="dropdown"])', function (e) {
     e.preventDefault();
     Menu.jump($(this).attr('href'));
 });
+
+$(document).on('submit', 'form#quest-create-form', function (e) {
+    e.preventDefault();
+    var form = new Form({
+        selector: '#' + $(this).attr('id'),
+        rules: [
+            {
+                name: 'name',
+                check: 'required',
+            }
+        ]
+    });
+    if (form.validate()) {
+        var quest = new Quest({
+            name: form.values.name,
+            version: '0.0.1'
+        });
+        localStorage.setItem('quest', quest);
+        Menu.jump('quest/info', {quest: localStorage.quest});
+    }
+    
+})
--- editor/js/editor_classes.js
+++ editor/js/editor_classes.js
@@ -1,4 +1,4 @@
-Menu = {
+var Menu = {
     jump: function (href, params)
     {
         
@@ -21,5 +21,80 @@ Menu = {
                 $('body').prepend(template.render());
             }
         });
+    }
+};
+
+var Editor = {
+    questLoaded: function ()
+    {
+        return (typeof localStorage.quest !== "undefined");
+    }
+};
+
+function Form(config)
+{
+    /**
+     * @type Object
+     */
+    this.rules = getFuncParam(config.rules, []);
+    /**
+     * @type String
+     */
+    this.selector = getFuncParam(config.selector, '');
+    /**
+     * @type Object
+     */
+    this.values = getFuncParam(config.values, $(this.selector).serializeObject());
+}
+
+Form.prototype.validate = function ()
+{
+    var success = true;
+    for (var key in this.rules) {
+         success = this._validateRule(this.rules[key]) && success;
+    }
+    return success;
+};
+
+Form.prototype._validateRule = function (rule)
+{
+    var value = this.values[rule.name];
+    switch (rule.check) {
+        case 'required':
+            if (value.length === 0) {
+                this._markError(rule);
+                return false;
+            } else {
+                this._clearError(rule);
+                return true;
+            }
+        default:
+            return false;
+    }
+};
+
+Form.prototype._findElement = function (name)
+{
+    return $(this.selector).find('[name="' + name + '"]');
+};
+
+Form.prototype._markError = function (rule)
+{
+    var element = this._findElement(rule.name);
+    element.closest('.form-group').addClass('has-error');
+    var messageBlock = $(element).nextAll('.validation-message-' + rule.check);
+    if (messageBlock.length == 0) {
+        var messageBlock = $('<div>').addClass('validation-message-' + rule.check + ' text-danger').text(getFuncParam(rule.message, 'Поле заполнено неправильно!'));
+    }
+    element.after(messageBlock);
+};
+
+Form.prototype._clearError = function (rule)
+{
+    var element = this._findElement(rule.name);
+    var messageBlock = $(element).nextAll('.validation-message-' + rule.check);
+    messageBlock.remove();
+    if (element.nextAll('.text-danger').length === 0) {
+        element.closest('.form-group').removeClass('has-error');
     }
 };
